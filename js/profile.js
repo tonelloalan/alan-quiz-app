f1d1cd0b9416f5c33947b13419f942233f99d0a1
ALAN TONELLO <alantb2@gmail.com>
2023-12-18 19:38:21 +0100

    create variables for the dark mode colors inside the stylesheet

--- js/profile.js
+++ js/profile.js
@@ -1,1 +1,4 @@
-profile.js;
+// Add an eventlistener to the darkmode toggle button in your profile page which should toggle on a dark or light version of the profile page
+
+// ❗️ You'll need to set some CSS variables as attributes to the element for example
+// ❗️ This functionality should only work for the profile page. We will have a look later how to implement dark/lightmode on the whole application later in the course.
