98885a3e9ab0de7d9e4ce7c8181be5ed0036a07e
ALAN TONELLO <alantb2@gmail.com>
2023-12-18 11:43:04 +0100

    end first step of toggle bookmark image

--- js/index.js
+++ js/index.js
@@ -1,2 +1,13 @@
 const bookmarkIcon = document.querySelector("[data-js=bookmark-icon]");
-console.log(bookmarkIcon);
+// console.log(bookmarkIcon);
+
+bookmarkIcon.addEventListener("click", function () {
+  // Check the current source of the bookmark icon
+  if (bookmarkIcon.src.endsWith("/resources/bookmark.png")) {
+    //Change the source to the filled bookmark icon
+    bookmarkIcon.src = "./resources/bookmark_filled.png";
+  } else {
+    // Change the source back to the regular bookmark icon
+    bookmarkIcon.src = "./resources/bookmark.png";
+  }
+});
