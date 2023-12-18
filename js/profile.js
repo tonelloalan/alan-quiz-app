f4e055d60cf4abc8d7bb1289672b4519bbf0a002
ALAN TONELLO <alantb2@gmail.com>
2023-12-18 20:23:46 +0100

    finished dm toggle

--- js/profile.js
+++ js/profile.js
@@ -1,4 +1,7 @@
 // Add an eventlistener to the darkmode toggle button in your profile page which should toggle on a dark or light version of the profile page
 
-// ❗️ You'll need to set some CSS variables as attributes to the element for example
-// ❗️ This functionality should only work for the profile page. We will have a look later how to implement dark/lightmode on the whole application later in the course.
+const darkModeToggle = document.querySelector(".switch input");
+
+darkModeToggle.addEventListener("change", function () {
+  document.body.classList.toggle("darkMode", this.checked);
+});
