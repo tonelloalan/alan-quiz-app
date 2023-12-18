9866e5369c157d784fcaf2c17194cacee7831b8d
ALAN TONELLO <alantb2@gmail.com>
2023-12-18 16:17:17 +0100

    assign the values of the form entries to variables

--- js/form.js
+++ js/form.js
@@ -2,5 +2,15 @@ const quizForm = document.getElementById("quiz-form");
 
 quizForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  console.log(event.target.elements);
+
+  const formElements = event.target.elements;
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  const formQuestionInput = data.formQuestion;
+  const formAnswerInput = data.formAnswer;
+  const formTagInput = data.formTag;
+
+  console.log(data.formQuestion, data.formAnswer, data.formTag); // prints the text entered on the "Your question" form
+
+  //   console.log(event.target.elements);
 });
