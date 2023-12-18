e6ebb42ce4c315a9f05d6c828301dc41791ea4d7
ALAN TONELLO <alantb2@gmail.com>
2023-12-18 15:55:20 +0100

    print data from event target elements

--- js/form.js
+++ js/form.js
@@ -2,4 +2,5 @@ const quizForm = document.getElementById("quiz-form");
 
 quizForm.addEventListener("submit", function (event) {
   event.preventDefault();
+  console.log(event.target.elements);
 });
