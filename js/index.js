a65a738ebc81890f7db95d8db5a77a7cdb3d4cf1
ALAN TONELLO <alantb2@gmail.com>
2023-12-18 12:42:58 +0100

    add if else to button text

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const bookmarkIcon = document.querySelector("[data-js=bookmark-icon]");
 // console.log(bookmarkIcon);
+const answerButton = document.querySelector("[data-js=answer-button]");
 
 bookmarkIcon.addEventListener("click", function () {
   // Check the current source of the bookmark icon
@@ -9,5 +10,19 @@ bookmarkIcon.addEventListener("click", function () {
   } else {
     // Change the source back to the regular bookmark icon
     bookmarkIcon.src = "./resources/bookmark.png";
+  }
+});
+
+answerButton.addEventListener("click", function () {
+  // Get the h4 element inside the question card
+  const answerText = document.querySelector(".question-cards h4");
+
+  // Toggle the 'hidden' class on the h4 element
+  answerText.classList.toggle("hidden");
+
+  if (answerButton.innerText === "Show Answer") {
+    answerButton.innerText = "Hide Answer";
+  } else {
+    answerButton.innerText = "Show Answer";
   }
 });
