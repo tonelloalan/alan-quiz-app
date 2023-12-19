cac37e9fe517642425a3b74386f28023ac5ab05d
ALAN TONELLO <alantb2@gmail.com>
2023-12-19 09:38:31 +0100

    update images to white when dark mode is active

--- js/profile.js
+++ js/profile.js
@@ -4,5 +4,19 @@ const darkModeToggle = document.querySelector(".switch input"); // targeting the
 const imgElements = document.querySelectorAll(".nav-bar img"); // targeting the nav bar images with a new variable
 
 darkModeToggle.addEventListener("change", function () {
+  // Toggle dark mode class on body
   document.body.classList.toggle("darkMode", this.checked);
+
+  // Change src attribute of img elements in the nav-bar
+  imgElements.forEach((img) => {
+    const currentSrc = img.src;
+    const fileExtensionIndex = currentSrc.lastIndexOf(".");
+    const baseFileName = currentSrc.substring(0, fileExtensionIndex);
+    const fileExtension = currentSrc.substring(fileExtensionIndex);
+    const newSrc = this.checked
+      ? baseFileName + "_white" + fileExtension
+      : baseFileName.replace("_white", "") + fileExtension;
+
+    img.src = newSrc;
+  });
 });
