d683c140b90d6bc9c15416f2058639e9158c613f
ALAN TONELLO <alantb2@gmail.com>
2023-12-19 09:34:26 +0100

    target images via new variable

--- js/profile.js
+++ js/profile.js
@@ -1,6 +1,7 @@
 // Add an eventlistener to the darkmode toggle button in your profile page which should toggle on a dark or light version of the profile page
 
-const darkModeToggle = document.querySelector(".switch input");
+const darkModeToggle = document.querySelector(".switch input"); // targeting the switch button with a new variable
+const imgElements = document.querySelectorAll(".nav-bar img"); // targeting the nav bar images with a new variable
 
 darkModeToggle.addEventListener("change", function () {
   document.body.classList.toggle("darkMode", this.checked);
